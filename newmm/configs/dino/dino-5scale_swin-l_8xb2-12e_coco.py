162ac5bf350f558113d7f56d3bbff0fb7c40fccb
Ryan <rsl82@rutgers.edu>
2024-01-10 18:38:36 +0900

    feat: new features on newmm
    #28

--- newmm/configs/dino/dino-5scale_swin-l_8xb2-12e_coco.py
+++ newmm/configs/dino/dino-5scale_swin-l_8xb2-12e_coco.py
@@ -28,3 +28,5 @@ model = dict(
     neck=dict(in_channels=[192, 384, 768, 1536], num_outs=num_levels),
     encoder=dict(layer_cfg=dict(self_attn_cfg=dict(num_levels=num_levels))),
     decoder=dict(layer_cfg=dict(cross_attn_cfg=dict(num_levels=num_levels))))
+
+load_from = '/data/ephemeral/home/level2-objectdetection-cv-03/newmm/dino-5scale_swin-l_8xb2-12e_coco_20230228_072924-a654145f.pth'
---
+++ newmm/predino.py
@@ -0,0 +1,17 @@
+_base_ = './configs/dino/dino-5scale_swin-l_8xb2-12e_coco.py'
+
+model = dict(bbox_head=dict(num_classes=10))
+
+vis_backends = [
+    dict(type='WandbVisBackend',
+         init_kwargs={
+             'project':'newmmdetection',
+             'entity':'ai-tech-6th-funfun',
+             
+         })
+]
+visualizer = dict(type='DetLocalVisualizer',vis_backends=vis_backends,name='visualizer')
+
+
+default_hooks = dict(checkpoint=dict(type='CheckpointHook',interval=1,save_best='auto',max_keep_ckpts=3))
+custom_hooks = [dict(type='SubmissionHook')]
