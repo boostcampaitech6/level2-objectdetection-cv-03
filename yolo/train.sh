1783fa12a5c7fb59a0f2ced40c4862a24491544a
rudeuns <rudeuns@gmail.com>
2024-01-22 14:34:13 +0900

    chore: Remove files
    #41

--- yolo/train.sh
+++
@@ -1,5 +0,0 @@
-python ./train.py --model yolov8s.pt --data /data/ephemeral/home/level2-objectdetection-cv-03/yolo/datasets/recycle_det_42_1.yaml --name yolov8s_42_1_e50_SGD;
-python ./train.py --model yolov8s.pt --data /data/ephemeral/home/level2-objectdetection-cv-03/yolo/datasets/recycle_det_42_2.yaml --name yolov8s_42_2_e50_SGD;
-python ./train.py --model yolov8s.pt --data /data/ephemeral/home/level2-objectdetection-cv-03/yolo/datasets/recycle_det_42_3.yaml --name yolov8s_42_3_e50_SGD;
-python ./train.py --model yolov8s.pt --data /data/ephemeral/home/level2-objectdetection-cv-03/yolo/datasets/recycle_det_42_4.yaml --name yolov8s_42_4_e50_SGD;
-python ./train.py --model yolov8s.pt --data /data/ephemeral/home/level2-objectdetection-cv-03/yolo/datasets/recycle_det_42_5.yaml --name yolov8s_42_5_e50_SGD;
